ebafdaababc390805fa20dc5bd17d7cb79997044
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-04-25 11:20:51 +0100

    Complete Storyboard Tasks.

---
+++ SHIELDHeroTracker/HeroDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HeroDetailViewController.swift
+//  SHIELDHeroTracker
+//
+//  Created by Belén Molina del Campo on 25/04/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+import UIKit
+
+class HeroDetailViewController: UIViewController {
+    
+    @IBOutlet weak var heroNameLabel: UILabel!
+    @IBOutlet weak var heroHomeworldLabel: UILabel!
+    @IBOutlet weak var heroSuperpowersLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
